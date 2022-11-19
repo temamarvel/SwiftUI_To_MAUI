db6c218cdd6233d9bd635eef253e5f0069c1706b
artem.denisov <artem.denisov@devexpress.com>
2022-11-19 16:33:29 +0400

    create and setup SwiftUI framework

---
+++ SwiftUI_MAUI_Framework/SwiftUI_MAUI_Framework/HelloWorldView.swift
@@ -0,0 +1,21 @@
+//
+//  HelloWorldView.swift
+//  SwiftUI_MAUI_Framework
+//
+//  Created by Art Denisov on 19.11.2022.
+//
+
+import SwiftUI
+
+struct HelloWorldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .background(.orange)
+    }
+}
+
+struct HelloWorldView_Previews: PreviewProvider {
+    static var previews: some View {
+        HelloWorldView()
+    }
+}
