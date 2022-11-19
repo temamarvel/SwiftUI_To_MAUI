b4e9abb45163378fc308094eafa1a56940a96946
temamarvel <tema.denisoff@gmail.com>
2022-11-19 23:20:32 +0400

    add and setup test MAUI App

---
+++ MAUI_App/MAUI_App/AppShell.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace MAUI_App;
+
+public partial class AppShell : Shell {
+    public AppShell() {
+        InitializeComponent();
+    }
+}
---
+++ MAUI_App/MAUI_App/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using MAUI_Library;
+using Microsoft.Extensions.Logging;
+
+namespace MAUI_App;
+
+public static class MauiProgram {
+    public static MauiApp CreateMauiApp() {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            }).ConfigureMauiHandlers(handlers =>
+            {
+                handlers.AddHandler(typeof(HelloWorldControl), typeof(HelloWorldHandler));
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
