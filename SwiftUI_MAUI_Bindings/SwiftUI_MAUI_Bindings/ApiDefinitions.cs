8f60b184cdfcd4ad1f7e2dbbee54b70c1ccca16c
temamarvel <tema.denisoff@gmail.com>
2022-11-25 23:44:34 +0400

    update ApiDefinitions for Bindings library

--- SwiftUI_MAUI_Bindings/SwiftUI_MAUI_Bindings/ApiDefinitions.cs
+++ SwiftUI_MAUI_Bindings/SwiftUI_MAUI_Bindings/ApiDefinitions.cs
@@ -1,3 +1,4 @@
+using System;
 using Foundation;
 using ObjCRuntime;
 using UIKit;
@@ -8,6 +9,31 @@ namespace SwiftUI_MAUI_Framework
 	[BaseType (typeof(NSObject), Name = "_TtC22SwiftUI_MAUI_Framework17HelloWorldWrapper")]
 	interface HelloWorldWrapper
 	{
+		// @property (readonly, nonatomic, strong) UIView * _Nullable uiView;
+		[NullAllowed, Export ("uiView", ArgumentSemantic.Strong)]
+		UIView UiView { get; }
+	}
+
+	// @interface SpinEditWrapper : NSObject
+	[BaseType (typeof(NSObject), Name = "_TtC22SwiftUI_MAUI_Framework15SpinEditWrapper")]
+	interface SpinEditWrapper
+	{
+		// @property (nonatomic) NSDecimal value;
+		[Export ("value", ArgumentSemantic.Assign)]
+		NSDecimal Value { get; set; }
+
+		// @property (nonatomic) NSDecimal interval;
+		[Export ("interval", ArgumentSemantic.Assign)]
+		NSDecimal Interval { get; set; }
+
+		// @property (copy, nonatomic) void (^ _Nullable)(NSDecimal) onValueChanged;
+		[NullAllowed, Export ("onValueChanged", ArgumentSemantic.Copy)]
+		Action<NSDecimal> OnValueChanged { get; set; }
+
+		// @property (copy, nonatomic) void (^ _Nullable)(NSDecimal) onIntervalChanged;
+		[NullAllowed, Export ("onIntervalChanged", ArgumentSemantic.Copy)]
+		Action<NSDecimal> OnIntervalChanged { get; set; }
+
 		// @property (readonly, nonatomic, strong) UIView * _Nullable uiView;
 		[NullAllowed, Export ("uiView", ArgumentSemantic.Strong)]
 		UIView UiView { get; }
