6d9d434590f1f6f8f28b5c18a52476a6cf41558e
temamarvel <tema.denisoff@gmail.com>
2022-12-12 23:47:17 +0400

    implement Buttons property for MAUI

--- MAUI_Library/MAUI_Library/Controls/SegmentedButtonsControl.cs
+++ MAUI_Library/MAUI_Library/Controls/SegmentedButtonsControl.cs
@@ -1,11 +1,24 @@
 using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using SwiftUI_MAUI_Framework;
 
 namespace MAUI_Library;
 
 public class SegmentedButtonsControl : View {
     public static readonly BindableProperty SelectedIndicesProperty = BindableProperty.Create(nameof(SelectedIndices), typeof(List<int>), typeof(SegmentedButtonsControl), new List<int>());
     public static readonly BindableProperty AllowsMultipleSelectionProperty = BindableProperty.Create(nameof(AllowsMultipleSelection), typeof(bool), typeof(SegmentedButtonsControl), default);
-    
+    public static readonly BindableProperty ButtonsProperty = BindableProperty.Create(nameof(Buttons), typeof(ObservableCollection<ButtonDescription>), typeof(SegmentedButtonsControl), new ObservableCollection<ButtonDescription>());
+
+    public SegmentedButtonsControl() {
+        Buttons.CollectionChanged += ButtonsOnCollectionChanged;
+    }
+
+    private void ButtonsOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs args) {
+        Handler?.Invoke(nameof(SegmentedButtonsControl.Buttons.CollectionChanged), args);
+    }
+
     public List<int> SelectedIndices {
         get => (List<int>)GetValue(SelectedIndicesProperty);
         set => SetValue(SelectedIndicesProperty, value);
@@ -14,5 +27,10 @@ public class SegmentedButtonsControl : View {
     public bool AllowsMultipleSelection {
         get => (bool)GetValue(AllowsMultipleSelectionProperty);
         set => SetValue(AllowsMultipleSelectionProperty, value);
+    }
+    
+    public ObservableCollection<ButtonDescription> Buttons {
+        get => (ObservableCollection<ButtonDescription>)GetValue(ButtonsProperty);
+        set => SetValue(ButtonsProperty, value);
     }
 }
--- MAUI_Library/MAUI_Library/Platforms/iOS/iOSSegmentedButtonsHandler.cs
+++ MAUI_Library/MAUI_Library/Platforms/iOS/iOSSegmentedButtonsHandler.cs
@@ -1,4 +1,3 @@
-using Foundation;
 using Microsoft.Maui.Handlers;
 using SwiftUI_MAUI_Framework;
 using UIKit;
@@ -8,14 +7,19 @@ namespace MAUI_Library;
 public partial class SegmentedButtonsHandler : ViewHandler<SegmentedButtonsControl, UIView> {
     private SegmentedButtonsWrapper Wrapper { get; set; }
 
-    public SegmentedButtonsHandler() : base(PropertyMapper) {
+    public SegmentedButtonsHandler() : base(PropertyMapper, CommandMapper) {
     }
 
     private static IPropertyMapper<SegmentedButtonsControl, SegmentedButtonsHandler> PropertyMapper =
         new PropertyMapper<SegmentedButtonsControl, SegmentedButtonsHandler>(ViewMapper) {
             [nameof(SegmentedButtonsControl.SelectedIndices)] = MapSelectedIndices,
             [nameof(SegmentedButtonsControl.AllowsMultipleSelection)] = MapAllowsMultipleSelection,
+            [nameof(SegmentedButtonsControl.Buttons)] = MapButtons,
         };
+
+    private static void MapButtons(SegmentedButtonsHandler handler, SegmentedButtonsControl control) {
+        handler.Wrapper.Buttons = control.Buttons.ToArray();
+    }
 
     private static void MapAllowsMultipleSelection(SegmentedButtonsHandler handler, SegmentedButtonsControl control) {
         handler.Wrapper.AllowsMultipleSelection = control.AllowsMultipleSelection;
@@ -23,6 +27,14 @@ public partial class SegmentedButtonsHandler : ViewHandler<SegmentedButtonsContr
 
     private static void MapSelectedIndices(SegmentedButtonsHandler handler, SegmentedButtonsControl control) {
         handler.Wrapper.SelectedIndices = control.SelectedIndices.ToNSNumbersArray();
+    }
+
+    public static CommandMapper<SegmentedButtonsControl, SegmentedButtonsHandler> CommandMapper = new(ViewCommandMapper) {
+        [nameof(SegmentedButtonsControl.Buttons.CollectionChanged)] = MapButtonsCollectionChanged,
+    };
+
+    private static void MapButtonsCollectionChanged(SegmentedButtonsHandler handler, SegmentedButtonsControl control, object args) {
+        handler.Wrapper.Buttons = control.Buttons.ToArray();
     }
 
     protected override UIView CreatePlatformView() {
